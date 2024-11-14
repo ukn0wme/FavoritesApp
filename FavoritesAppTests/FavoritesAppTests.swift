895c9e9ae5f56c6779da65a2a4e625c4449b622c
ukn0wme <ukn0wme.design@gmail.com>
2024-11-15 03:55:34 +0500

    Initial Commit

---
+++ FavoritesAppTests/FavoritesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesAppTests.swift
+//  FavoritesAppTests
+//
+//  Created by Daniyal Nurgazinov on 15.11.2024.
+//
+
+import Testing
+@testable import FavoritesApp
+
+struct FavoritesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
